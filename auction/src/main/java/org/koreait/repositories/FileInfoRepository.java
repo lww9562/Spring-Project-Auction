2233075790492df597db795a4c6e74836cd51248
lww9562 <lww9562@naver.com>
2023-05-11 08:53:46 +0900

    파일 자동삭제 스케쥴러 추가

--- auction/src/main/java/org/koreait/repositories/FileInfoRepository.java
+++ auction/src/main/java/org/koreait/repositories/FileInfoRepository.java
@@ -1,15 +1,22 @@
 package org.koreait.repositories;
 
 import org.koreait.entities.FileInfo;
+import org.koreait.entities.QFileInfo;
+import org.koreait.entities.QProducts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
 public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
+    default boolean exists(Long fileNo){
+        QFileInfo fileInfo = QFileInfo.fileInfo;
+        return exists(fileInfo.fileNo.eq(fileNo));
+
+    }
+
     FileInfo findByFileNo(Long fileNo);
 
     List<FileInfo> findByGidOrderByRegDtAsc(String gid);
-    List<FileInfo> deleteBySuccess(Boolean success);
     List<FileInfo> findBySuccess(Boolean success);
 }
